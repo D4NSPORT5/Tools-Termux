1fdd23a22b392eb0426e24d905067edaa08d4efe
FISHSCALE <46251304+D4NSPORT5@users.noreply.github.com>
2019-05-02 20:21:31 +0300

    Update tools-FISHSCALE.py

--- tools-FISHSCALE.py
+++ tools-FISHSCALE.py
@@ -7,7 +7,7 @@ Ver: 1.0
 TOOLS ONLY FOR TERMUX!
 ----------------------
 """)
-lang = input('Eng/Rus? ')
+lang = input('Eng/Рус? ')
 
 def menu():
 	
@@ -36,7 +36,7 @@ def menu():
 ------------------------------------------
 99. Exit
 """)
-	elif (lang == "Rus") or (lang == "rus"):
+	elif (lang == "Рус") or (lang == "рус"):
 		print("""
 00. Сделать ваш Android хакерским.
 ------------------------------------------
@@ -63,7 +63,7 @@ def menu():
 """)
 
 def suc(a, b):
-	if (lang == "Rus") or (lang == "rus"):		
+	if (lang == "рус") or (lang == "Рус"):		
 		print('=======================')
 		print(a, 'успешна скачана!')
 		print(b)
@@ -87,7 +87,7 @@ while loop == True:
 		os.system("pkg update -y")
 		os.system("pkg install -y nmap")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Nmap', 'Выйди из программы и напиши "cd", потом "Nmap"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Nmap', 'Exit the program and write "cd", after "Nmap"')
@@ -97,7 +97,7 @@ while loop == True:
 		os.system("pkg update -y")
 		os.system("pkg install -y hydra")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Hydra', 'Выйди из программы и напиши "cd", потом "Hydra"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Hydra', 'Exit the program and write "cd", after "Hydra"')
@@ -109,7 +109,7 @@ while loop == True:
 		os.system("pkg install python2")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('SQLmap', 'Выйдите из программы и напишите "cd", после "python2 sqlmap.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('SQLmap', 'Exit the program and write "cd", after "python2 sqlmap.py"')
@@ -123,7 +123,7 @@ while loop == True:
 		os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
 		os.system("cd /data/data/com.termux/files/home && bundle install")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Metasploit', 'Выйдите из программы и напишите "cd", после "msfconsole"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Metasploit', 'Exit the program and write "cd", after "msfconsole"')
@@ -133,7 +133,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Ngrok', 'Выйдите из программы и напишите "cd", после "./ngrok http 80"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Ngrok', 'Exit the program and write "cd", after "./ngrok http 80"')	
@@ -143,7 +143,7 @@ while loop == True:
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Hax4us/Nethunter-In-Termux.git")
 		os.system("cd /data/data/com.termux/files/home && cd Nethunter-In-Termux && chmod +x kalinethunter")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Nethunter', 'Выйдите из программы и напишите "cd", после "сd Nethunter-In-Termux", после этого "./kalinethunter"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Nethunter', 'Exit the program and write "cd", after "cd Nethunter-In-Termux", after that "./kalinethunter"')
@@ -157,7 +157,7 @@ while loop == True:
 		os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
 		os.system("cd /data/data/com.termux/files/home && pip2 install requests")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('angryFuzzer', 'Выйдите из программы и напишите "cd", после "сd angryFuzzer", после этого "python2 angryFuzzer.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('angryFuzzer', 'Exit the program and write "cd", after "cd angryFuzzer", after that "python2 angryFuzzer.py"')
@@ -168,7 +168,7 @@ while loop == True:
 		os.system("pkg install -y php")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('RED_HAWK', 'Выйдите из программы и напишите "cd", после "сd RED_HAWK", после этого "php rhawk.php"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('RED_HAWK', 'Exit the program and write "cd", after "cd RED_HAWK", after that "php rhawk.php"')
@@ -181,7 +181,7 @@ while loop == True:
 		os.system("cd /data/data/com.termux/files/home && cd weeman")
 		os.system("cd /data/data/com.termux/files/home && chmod +x weeman.py")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('weeman', 'Выйдите из программы и напишите "cd", после "сd weeman", после этого "python2 weeman.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('weeman', 'Exit the program and write "cd", after "cd weeman", after that "python2 weeman.py"')
@@ -194,7 +194,7 @@ while loop == True:
 		os.system("cd /data/data/com.termux/files/home && cd IPGeoLocation")
 		os.system("cd /data/data/com.termux/files/home && pip install -r requirements.txt")
 		os.system("cd /data/data/com.termux/files/home")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('IPGeoLocation', 'Выйдите из программы и напишите "cd", после "сd IPGeoLocation", после этого "python ipgeolocation.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('IPGeoLocation', 'Exit the program and write "cd", after "cd IPGeoLocation", after that "python ipgeolocation.py"')
@@ -204,7 +204,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("pkg install -y python")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Cupp', 'Выйдите из программы и напишите "cd", после "сd Cupp", после этого "python cupp3.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Cupp', 'Exit the program and write "cd", after "cd Cupp", after that "python cupp3.py"')
@@ -217,7 +217,7 @@ while loop == True:
 		os.system("pip install requests")
 		os.system("pip install beautifulsoup4")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Instahack', 'Выйдите из программы и напишите "cd", после "сd Instahack", после этого "python hackinsta.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Instahack', 'Exit the program and write "cd", after "cd Instahack", after that "python hackinsta.py"')
@@ -229,7 +229,7 @@ while loop == True:
 		os.system("pip install mechanicalsoup")
 		os.system("pkg install -y nano")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('TwitterSniper', 'Выйдите из программы и напишите "cd", после "сd TwitterSniper", после этого "python TwitterSniper.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('TwitterSniper', 'Exit the program and write "cd", after "cd TwitterSniper", after that "python TwitterSniper.py"')
@@ -239,7 +239,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Neo-Oli/termux-ubuntu.git")
 		os.system("cd /data/data/com.termux/files/home && cd termux-ubuntu && bash ubuntu.sh")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Ubuntu', 'Выйдите из программы и напишите "cd", после "сd termux-ubuntu", после этого "./start.sh"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Ubuntu', 'Exit the program and write "cd", after "cd termux-ubuntu", after that "./start.sh"')
@@ -249,7 +249,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("pkg install -y wget")
 		os.system("apt update && apt install wget -y && /data/data/com.termux/files/usr/bin/wget https://raw.githubusercontent.com/nmilosev/termux-fedora/master/termux-fedora.sh")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Fedora', 'Выйдите из программы и напишите "cd", после этого "sh termux-fedora.sh f26_arm64" или "sh termux-fedora.sh f26_arm"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Fedora', 'Exit the program and write "cd", after that "sh termux-fedora.sh f26_arm64" or "sh termux-fedora.sh f26_arm"')
@@ -259,7 +259,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("pkg install -y python2")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('viSQL', 'Выйдите из программы и напишите "cd", после "сd viSQL", после этого "python2 viSQL.py --help"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('viSQL', 'Exit the program and write "cd", after "cd viSQL", after that "python2 viSQL.py --help"')
@@ -269,7 +269,7 @@ while loop == True:
 		os.system("pkg install -y git")
 		os.system("pkg install -y python2")
 		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
 			suc('Hash-Buster', 'Выйдите из программы и напишите "cd", после "сd Hash-Buster", после этого "python2 hash.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('Hash-Buster', 'Exit the program and write "cd", after "cd Hash-Buster", after that "python2 hash.py"')
@@ -285,7 +285,112 @@ while loop == True:
 		os.system("pip2 install -r requirements.txt")
 		os.system("pip2 install -r requirements-dev.txt")
 		os.system("pip2 install -r requests")
-		if (lang == 'rus') or (lang == 'Rus'):
+		if (lang == "рус") or (lang == "Рус"):
+			suc('routersploit', 'Выйдите из программы и напишите "cd", после "сd routersploit", после этого "python2 rsf.py"')
+		elif (lang == 'eng') or (lang == 'Eng'):
+			suc('routersploit', 'Exit the program and write "cd", after "cd routersploit", after that "python2 rsf.py"')
+	#00===========================
+	if kavo == 00:
+		if (lang == "Eng") or (lang == "eng"):
+			print("===========================")
+			print("Please put down your Android and go have some tea...")
+			print("(the process is very long)")
+			print("===========================")
+		else:
+			print("===========================")
+			print("Пожалуйста, положи свой Андроид и иди попей чаю...")
+			print("(процесс очень долгий)")
+			print("===========================")
+		os.system("pkg update")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python")
+		os.system("pkg install -y python2")
+		os.system("pkg install -y wget")
+		os.sysetm("pkg install -y nmap")
+		os.system("plg install -y hydra ")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/sqlmapproject/sqlmap.git")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg install wget")
+		os.system("cd /data/data/com.termux/files/home && wget https://Auxilus.github.io/metasploit.sh")
+		os.system("cd /data/data/com.termux/files/home && bash metasploit.sh")
+		os.system("cd /data/data/com.termux/files/home && cd metasploit-framework")
+		os.system("cd /data/data/com.termux/files/home && gem install bundle --pre")
+		os.system("cd /data/data/com.termux/files/home && bundle config build.nokogiri --use-system-libraries")
+		os.system("cd /data/data/com.termux/files/home && bundle install")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/themastersunil/ngrok.git")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/ihebski/angryFuzzer.git")
+		os.system("cd /data/data/com.termux/files/home && cd angryFuzzer")
+		os.system("cd /data/data/com.termux/files/home && pip2 install -r requirements.txt")
+		os.system("cd /data/data/com.termux/files/home && pip2 install requests")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y php")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Tuhinshubhra/RED_HAWK")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y python2")
+		os.system("pkg install -y git")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/evait-security/weeman.git")
+		os.system("cd /data/data/com.termux/files/home && cd weeman")
+		os.system("cd /data/data/com.termux/files/home && chmod +x weeman.py")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/maldevel/IPGeoLocation.git")
+		os.system("cd /data/data/com.termux/files/home && cd IPGeoLocation")
+		os.system("cd /data/data/com.termux/files/home && pip install -r requirements.txt")
+		os.system("cd /data/data/com.termux/files/home")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/Mebus/cupp.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python")
+		os.system("pkg install -y nano")
+		os.system("pip install requests")
+		os.system("pip install beautifulsoup4")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/avramit/instahack.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python")
+		os.system("pip install mechanicalsoup")
+		os.system("pkg install -y nano")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/abdallahelsokary/TwitterSniper.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/blackvkng/viSQL.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/UltimateHackers/Hash-Buster.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/shawarkhanethicalhacker/D-TECT.git")
+		os.system("pkg update -y")
+		os.system("pkg install -y git")
+		os.system("pkg install -y python2")
+		os.system("cd /data/data/com.termux/files/home && git clone https://github.com/reverse-shell/routersploit.git")
+		os.system("cd /data/data/com.termux/files/home && cd routersploit")
+		os.system("pip2 install -r requirements.txt")
+		os.system("pip2 install -r requirements-dev.txt")
+		os.system("pip2 install -r requests")
+		os.system("clear")
+		if (lang == "рус") or (lang == "Рус"):
 			suc('routersploit', 'Выйдите из программы и напишите "cd", после "сd routersploit", после этого "python2 rsf.py"')
 		elif (lang == 'eng') or (lang == 'Eng'):
 			suc('routersploit', 'Exit the program and write "cd", after "cd routersploit", after that "python2 rsf.py"')
@@ -294,11 +399,9 @@ while loop == True:
 		if (lang == "eng") or (lang == "Eng"):	
 			print("Bye! Happy Hacking)")
 			loop = False
-		elif (lang == "Rus") or (lang == "rus"):
+		elif (lang == "рус") or (lang == "Рус"):
 			print("Пока! Счастливого хакинга)")
 			loop = False
-	if kavo == 99:
-		loop = False
 	else:
 		otv = input("#: ")
 		if (otv == "n") or (otv == "н"):	
